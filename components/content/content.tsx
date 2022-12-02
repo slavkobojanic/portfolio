264186661f18be7cfbd91bf6f151be840682b325
Slavko Bojanic <slavkobojj@gmail.com>
2022-12-03 00:34:10 +1100

    Added project table contents

--- components/content/content.tsx
+++ components/content/content.tsx
@@ -2,7 +2,9 @@ import styles from './content.module.css';
 import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md';
 import Image from 'next/image';
 import imgPersonal from 'public/img_personal.jpeg';
-import { AiFillCaretDown } from 'react-icons/ai';
+import { AiFillCaretDown, AiOutlineEllipsis, AiOutlineUserAdd } from 'react-icons/ai';
+import { BiPlay } from 'react-icons/bi';
+import { HiDownload } from 'react-icons/hi';
 
 export const Content = () => {
   return (
@@ -34,8 +36,46 @@ export const Content = () => {
       </div>
 
       <div className={styles.contentProjects}>
+        <div className={styles.contentProjectsControls}>
+          <div className={styles.controlPlay}>
+            <BiPlay className={styles.controlPlayIcon}/>
+          </div>
+          <p>Enhance</p>
+          <div className={styles.controlDownload}>
+            <HiDownload className={styles.controlDownloadIcon}/>
+          </div>
+          <AiOutlineUserAdd size={38}/>
+          <AiOutlineEllipsis size={44}/>
+        </div>
 
+        <div className={styles.projectTableHeader}>
+          <p>#</p>
+          <p>Title</p>
+          <p>Date Added</p>
+          <p>View</p>
+        </div>
+
+        <div className={styles.projectTableRow}>
+          <p>1.</p>
+          <p>Portfolio</p>
+          <p>Dec 3, 2022</p>
+          <p>Link</p>
+        </div>
+
+        <div className={styles.projectTableRow}>
+          <p>2.</p>
+          <p>FFIITT - Macro Tracking</p>
+          <p>Aug 14, 2022</p>
+          <p>Link</p>
+        </div>
+
+        <div className={styles.projectTableRow}>
+          <p>3.</p>
+          <p>Horizon iTerm theme</p>
+          <p>Nov 20, 2018</p>
+          <p>Link</p>
+        </div>
       </div>
     </section>
   );
-}
+};
