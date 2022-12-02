97e9dd7af79f5e3d43b0a6be5ce9230c8b9edc6f
Slavko Bojanic <slavkobojj@gmail.com>
2022-12-03 00:55:43 +1100

    First attempt done

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -2,7 +2,6 @@ import styles from './menu.module.css';
 import { AiFillHeart, AiOutlineHome, AiOutlinePlus } from 'react-icons/ai';
 import { FiSearch } from 'react-icons/fi';
 import { VscLibrary } from 'react-icons/vsc';
-import { Spacer } from 'components/spacer/spacer';
 import React from 'react';
 import { BsFillBookmarkFill } from 'react-icons/bs';
 
@@ -15,81 +14,54 @@ export const Menu = () => {
         <div className={styles.macOSMaximise}></div>
       </div>
 
-      <div className={styles.scrollable}>
-        <nav className={styles.menuFake}>
-          <div className={styles.menuFakeLink}>
-            <AiOutlineHome size={27}/>
-            <p>Home</p>
-          </div>
-          <div className={styles.menuFakeLink}>
-            <FiSearch size={27}/>
-            <p>Search</p>
-          </div>
-          <div className={styles.menuFakeLink}>
-            <VscLibrary size={27}/>
-            <p>Your library</p>
-          </div>
-        </nav>
+      <nav className={styles.menuFake}>
+        <div className={styles.menuFakeLink}>
+          <AiOutlineHome size={27}/>
+          <p>Home</p>
+        </div>
+        <div className={styles.menuFakeLink}>
+          <FiSearch size={27}/>
+          <p>Search</p>
+        </div>
+        <div className={styles.menuFakeLink}>
+          <VscLibrary size={27}/>
+          <p>Your library</p>
+        </div>
+      </nav>
 
-        <Spacer/>
-
-        <nav className={styles.menuFake}>
-          <div className={styles.menuFakeLink}>
-            <div
-              className={styles.gradientMenuItem}
-              style={{background: `linear-gradient(135deg, #B2B2B2 0%, #B2B2B2 100%)`}}>
-              <AiOutlinePlus color="#000000"/>
-            </div>
-            <p>Create playlist</p>
+      <nav className={styles.menuFake}>
+        <div className={styles.menuFakeLink}>
+          <div
+            className={styles.gradientMenuItem}
+            style={{background: `linear-gradient(135deg, #B2B2B2 0%, #B2B2B2 100%)`}}>
+            <AiOutlinePlus color="#000000"/>
           </div>
-          <div className={styles.menuFakeLink}>
-            <div
-              className={styles.gradientMenuItem}
-              style={{background: `linear-gradient(135deg, #3214A3 0%, #889C99 100%)`}}>
-              <AiFillHeart color="#BBBBBB"/>
-            </div>
-            <p>Liked songs</p>
+          <p>Create playlist</p>
+        </div>
+        <div className={styles.menuFakeLink}>
+          <div
+            className={styles.gradientMenuItem}
+            style={{background: `linear-gradient(135deg, #3214A3 0%, #889C99 100%)`}}>
+            <AiFillHeart color="#BBBBBB"/>
           </div>
-          <div className={styles.menuFakeLink}>
-            <div
-              className={styles.gradientMenuItem}
-              style={{background: `linear-gradient(135deg, #1D4439 0%, #1D4439 100%)`}}>
-              <BsFillBookmarkFill color="#47944D"/>
-            </div>
-            <p>Saved episodes</p>
+          <p>Liked songs</p>
+        </div>
+        <div className={styles.menuFakeLink}>
+          <div
+            className={styles.gradientMenuItem}
+            style={{background: `linear-gradient(135deg, #1D4439 0%, #1D4439 100%)`}}>
+            <BsFillBookmarkFill color="#47944D"/>
           </div>
+          <p>Saved episodes</p>
+        </div>
+      </nav>
 
-          <div className={styles.hRule}/>
+      <div className={styles.hRule}/>
 
-          <div className={styles.blogLinks}>
-            <p>Euismod nisi porta lorem mollissssssss</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-            <p>Euismod nisi porta lorem mollis</p>
-          </div>
-
-          <div className={styles.blogLinksGradient}/>
-        </nav>
+      <div className={styles.blogLinks}>
+        <p>No blog posts yet!</p>
       </div>
+
     </section>
   );
 };
